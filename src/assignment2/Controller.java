8607295498e197d73635579076ecb39793248583
Kyle <Kyle@Kyle-PC.Home>
2018-05-01 09:38:53 +0100

    Initial version of assignment

---
+++ src/assignment2/Controller.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2;
+
+import java.awt.event.*;
+import javax.swing.Timer;
+
+public class Controller {
+    
+    ActionListener listener;
+    Timer timer;
+    
+    Model model;
+    View view;
+    
+    public Controller(Model m, View v) {
+        model = m;
+        view = v;
+        
+        listener = new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                model.update();
+            }
+        };
+        
+        timer = new Timer(100, listener);
+        timer.start();
+    }
+}
---
+++ src/assignment2/Model.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2;
+
+import java.util.Calendar;
+import java.util.Observable;
+//import java.util.GregorianCalendar;
+
+public class Model extends Observable {
+    
+    int hour = 0;
+    int minute = 0;
+    int second = 0;
+    
+    int oldSecond = 0;
+    
+    public Model() {
+        update();
+    }
+    
+    public void update() {
+        Calendar date = Calendar.getInstance();
+        hour = date.get(Calendar.HOUR);
+        minute = date.get(Calendar.MINUTE);
+        oldSecond = second;
+        second = date.get(Calendar.SECOND);
+        if (oldSecond != second) {
+            setChanged();
+            notifyObservers();
+        }
+    }
+}
